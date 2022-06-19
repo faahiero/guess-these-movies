72f4ad5c2edbbc002e57142c177fb0fba7174e12
Fabricio Silva <fabriciosilvalp@outlook.com>
2022-06-18 23:54:23 -0300

    First Commit

--- GuessMovie/ContentView.swift
+++ GuessMovie/ContentView.swift
@@ -6,11 +6,79 @@
 //
 
 import SwiftUI
+import AVFoundation
+
+
 
 struct ContentView: View {
+    
+    func speak(text: String){
+        let utterance = AVSpeechUtterance(string: text)
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+        let synthesizer = AVSpeechSynthesizer()
+        synthesizer.speak(utterance)
+        
+    }
+    
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Text("Guess These Movies")
+                .fontWeight(.bold)
+                .font(.system(.largeTitle, design: .rounded))
+            
+            Text("Can you guess the movies from these emojis?")
+            Text("Tap the button to find out the answer")
+            
+            
+            Button {
+                speak(text: "The answer is Back to the Future!")
+            } label: {
+                Text("👦🏻👴🏻🚗⌚️")
+                    .fontWeight(.bold)
+                    .font(.system(.title, design: .rounded))
+            }
             .padding()
+            .foregroundColor(.white)
+            .background(Color.green)
+            .cornerRadius(20)
+            
+            Button {
+                speak(text: "The answer is Frozen!")
+            } label: {
+                Text("🏰👭🌀❄️☃️")
+                    .fontWeight(.bold)
+                    .font(.system(.title, design: .rounded))
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.yellow)
+            .cornerRadius(20)
+            
+            Button {
+                speak(text: "The answer is Ocean 11!")
+            } label: {
+                Text("🌊1️⃣1️⃣")
+                    .fontWeight(.bold)
+                    .font(.system(.title, design: .rounded))
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.blue)
+            .cornerRadius(20)
+            
+            Button {
+                speak(text: "The answer is Spiderman!")
+            } label: {
+                Text("🕷🏃")
+                    .fontWeight(.bold)
+                    .font(.system(.title, design: .rounded))
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.brown)
+            .cornerRadius(20)
+        }
     }
 }
 
