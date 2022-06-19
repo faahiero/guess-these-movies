f21064e7da2743d34e235cb45652eb20ccb13e6a
Fabricio Silva <fabriciosilvalp@outlook.com>
2022-06-18 23:18:18 -0300

    Initial Commit

---
+++ GuessMovie/GuessMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessMovieApp.swift
+//  GuessMovie
+//
+//  Created by Fabricio Silva on 18/06/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
